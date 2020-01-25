9ad44e415da969284b70c26a67c2ab6b39e9c4ef
Rico Klimpel <rico@tklimpel.de>
2020-01-25 10:13:40 +0100

    never thought about underwater bicycles

--- binge.py
+++ binge.py
@@ -3,6 +3,17 @@
 import csv
 import matplotlib.pyplot as plt 
 import datetime
+import scipy.optimize as opt;
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter1d
+from scipy.interpolate import interp1d
+
+def read_data_from_file(file):
+    with open(file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        data = list(csv_reader)
+        data = data[1:]
+    return data
 
 def estimate_time(name):
     if "Staffel" in name:
@@ -13,12 +24,16 @@ def estimate_time(name):
     else:
         return 1.5
 
-# Read data from csv file into data list
-data = []
-with open('NetflixViewingHistory.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    data = list(csv_reader)
-    data = data[1:]
+def get_list_of_dates(start,end):
+    dates = []
+    delta = end - start
+    for i in range(delta.days + 1):
+        dates.append(start + datetime.timedelta(days=i))
+    return dates
+
+data = read_data_from_file('NetflixViewingHistory.csv')
+calendar = get_list_of_dates(datetime.datetime(2015, 6, 1,0,0,0),datetime.datetime.now())
+#calendar = get_list_of_dates(datetime.datetime(2016, 10, 7,0,0,0),datetime.datetime.now())
 
 # split for data and episode count lists
 dates = []
@@ -34,13 +49,6 @@ for log in data:
             dates.append(log[1])        
             episodes.append(estimate_time(log[0]))
 
-# Build a calendar of every date between start and end
-start = datetime.datetime(2015, 6, 1,0,0,0)
-end = datetime.datetime.now()
-delta = end - start
-calendar = []
-for i in range(delta.days + 1):
-    calendar.append(start + datetime.timedelta(days=i))
 
 #Convert dates from csv to date objects
 for i in range(len(dates)):
@@ -53,33 +61,55 @@ for day in calendar:
     for i in range(len(dates)):
         if(dates[i] == day):
             found = True
-            all_episodes.append(episodes[i])
+            all_episodes.append(int(episodes[i]))
     if not found:
-        all_episodes.append(0)
+        all_episodes.append(int(0))
 
-#Compress the data 
-compress_days = 7
+compress_days = 14
 dates_compressed = []
 episodes_compressed = []
-for i in range(len(dates)):
-    dates_compressed.append(dates[i])
+i = 0
+while i+compress_days < len(calendar):
+    print("i is " + str(i) + " handling dates " + str(calendar[i]) + " to " + str(calendar[i+compress_days]))
+    dates_compressed.append(calendar[i])
     value = 0
     for y in range(compress_days-1):
         value += all_episodes[i+y]
     episodes_compressed.append(value)
     i += compress_days
 
-#stacked_consume = []
-#for i in range(len(dates_compressed)):
-#    stacked_value = 0
-#    for k in range(i):
-#        stacked_value += episodes_compressed[k]
-#    stacked_consume.append(stacked_value)
-    
+for x in range(len(episodes_compressed)):
+    print(str(x) + ": " + str(dates_compressed[x]) + ": " + str(episodes_compressed[x]))
 
-plt.plot(dates_compressed, episodes_compressed)
-#plt.plot(dates_compressed, stacked_consume)
-plt.xlabel('Datum') 
-plt.ylabel('Konsum') 
-plt.title('Netflix Stats') 
+stacked_consume = []
+for i in range(len(dates_compressed)):
+    stacked_value = 0
+    for k in range(i):
+        stacked_value += episodes_compressed[k]
+    stacked_consume.append(stacked_value)
+
+date_number_array = range(0,len(episodes_compressed))
+
+x=np.array(date_number_array)
+y=np.array(episodes_compressed)
+
+x_new = np.linspace(x.min(), x.max(),500)
+
+f = interp1d(x, y, kind='quadratic')
+y_smooth=f(x_new)
+
+plt.title('Netflix Stats smoothed')
+plt.plot(x_new,y_smooth)
+#plt.scatter (x, y)
+plt.show()
+
+#plt.style.use('ggplot')
+#plt.plot(dates_compressed, ysmoothed)
+
+#plt.bar(dates_compressed, ysmoothed,40)
+
+plt.plot(dates_compressed, stacked_consume)
+#plt.xlabel('Datum') 
+#plt.ylabel('Konsum') 
+plt.title('Netflix Stats Stacked') 
 plt.show() 
--- readme.md
+++ readme.md
@@ -1,1 +1,15 @@
-![](./screenshot1.png)
+# Netflix Stats
+
+Short written skript trying to analyze netflix consumption data. You can get a csv of all watched movies and episodes under you netflix account section on the website. This file is analyzed here. I am trying to get a brief insight into plotting the graphs with python. You can see some of the results thrown together here. Watching time is roughly eastimated for only the shows i watched.
+
+I downloaded the data on the netflix website from here:
+
+![](./screenshots/screenshot6.png)
+
+The skript can show your consumption data as graph:
+
+![](./screenshots/screenshot5.png)
+
+Also it can view your total watching time:
+
+![](./screenshots/screenshot2.png)
